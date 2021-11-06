f9a6dc7a9a64f4cdbefd4c6971c1e25c81e3d0b3
melihyelman <myelman17@gmail.com>
2021-11-06 14:13:43 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'melih123'
+import 'melih123/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
